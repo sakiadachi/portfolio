a6dac5b7f3ac9b9789d57cd8f4bd5280c4476041
sakiadachi <58318131+sakiadachi@users.noreply.github.com>
2020-03-25 20:35:59 +0900

    second commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// const pageTopBtn = document.getElementById('pagetop');
+// pageTopBtn.addEventListener("click", function () {
+//   const me = arguments.callee;
+//   const nowY = window.pageYOffset;
+//   window.scrollTo(0, Math.floor(nowY * 0.8));
+//   if (nowY > 0) {
+//     window.setTimeout(me, 10);
+//   }
+// });
+
+var toTop = document.getElementById('toTop');
+
+window.onclick = function ()  {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.getElement.scrollTop > 20) {
+        toTop.style.display = "block";
+    } else {
+        toTop.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
